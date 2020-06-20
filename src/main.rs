2eb27343ae72ff101da6d2e22e9dde5b2f588830
rrokkam <rohithrokkam@gmail.com>
2020-06-20 16:55:26 -0700

    Create struct Program to represent bf turing-machine-like thing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,41 @@
+struct Program {
+    tape: Vec<u8>,
+    head: usize,
+    program: Vec<u8>,
+    pc: usize,
+}
+
+impl Program {
+    pub fn new(program: Vec<u8>) -> Self {
+        Program {
+            tape: Vec::with_capacity(30_000),
+            head: 0,
+            program: program,
+            pc: 0,
+        }
+    }
+
+    pub fn run(mut self) {
+        while !self.done() {
+            self.step()
+        }
+    }
+
+    fn step(&mut self) {
+        println!("READING: {}", self.program[self.pc] as char);
+        self.pc += 1;
+    }
+
+    pub fn done(&self) -> bool {
+        self.pc == self.program.len()
+    }
+}
+
 fn main() {
     let filename = std::env::args()
         .nth(1)
         .expect("args should have a filename as the first argument");
-    let program = std::fs::read_to_string(filename)
-        .expect("first argument in args should point to a valid utf8-encoded file");
-    print!("{}", program);
+    let program = std::fs::read(filename).expect("first argument in args should point to a file");
+
+    Program::new(program).run();
 }
